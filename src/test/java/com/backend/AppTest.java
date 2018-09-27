922f8ec0d739c8e73b38900c9bd76d9c7a193889
Ricardo Mendez Rico <ricardo2.22.08@hotmail.com>
2018-09-27 06:37:38 -0500

    added test that should abort when too many arguments are supplied. The test will fail due to no production code. Commit is only for TDD purposes. The following commits will be with test and production code.

--- src/test/java/com/backend/AppTest.java
+++ src/test/java/com/backend/AppTest.java
@@ -1,10 +1,16 @@
 package com.backend;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 
 public class AppTest 
@@ -12,6 +18,23 @@ public class AppTest
 
     @Rule
     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    private static final String EOL = System.getProperty("line.separator");
+    private PrintStream console;
+    private ByteArrayOutputStream bytes;
+
+    @Before
+    public void setUp() {
+        bytes   = new ByteArrayOutputStream();
+        console = System.out;
+        System.setOut(new PrintStream(bytes));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(console);
+    }
+
 
     @Test
     public void shouldAnswerWithTrue()
@@ -29,6 +52,13 @@ public class AppTest
     public void systemExitWithSelectedStatusCode0() {
         exit.expectSystemExitWithStatus(0);
         App.main();
+    }
+
+    @Test
+    public void testAbortWhenTooManyArgumentsSupplied() {
+        App.main("a", "b", "c");
+        assertEquals(App.MSG_TOO_MANY_ARGUMENTS + EOL +
+                App.USAGE + EOL, bytes.toString());
     }
 
 }
