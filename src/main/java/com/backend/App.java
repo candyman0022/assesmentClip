efeab2db61311a062ec659406df78111f41647f5
Ricardo Mendez Rico <ricardo2.22.08@hotmail.com>
2018-09-27 06:14:43 -0500

    add maven dependency for testing system exit codes. changed main signature to receive var args instead of an array of args test for checking system.exit(0)

--- src/main/java/com/backend/App.java
+++ src/main/java/com/backend/App.java
@@ -2,8 +2,8 @@ package com.backend;
 
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String... args )
     {
-
+        System.exit(0);
     }
 }
--- src/test/java/com/backend/AppTest.java
+++ src/test/java/com/backend/AppTest.java
@@ -2,19 +2,33 @@ package com.backend;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+
+    @Test
+    public void systemExitWithArbitraryStatusCode() {
+        exit.expectSystemExit();
+        App.main();
+    }
+
+    @Test
+    public void systemExitWithSelectedStatusCode0() {
+        exit.expectSystemExitWithStatus(0);
+        App.main();
+    }
+
 }
